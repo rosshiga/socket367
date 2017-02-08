352d781c0784f26b4378c1fc84ace1c2ea31eb3b
RossHiga <commits_n_gits@rosshiga.com>
2017-02-07 18:11:16 -1000

    Added check command

--- server.c
+++ server.c
@@ -15,138 +15,221 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3527"  // the port users will be connecting to
+#define PORT "3521"  // the port users will be connecting to
 
-#define BACKLOG 10	 // how many pending connections queue will hold
+#define BACKLOG 10   // how many pending connections queue will hold
 
 void sigchld_handler(int s)
 {
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+  while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+
+//Child process
+void error(char *s);
+
+
+
+
+
+
 
 int main(void)
 {
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
+  int sockfd, new_fd, numbytes;  // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+  char rec_cmd[100];
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
+  //For child Process
+  int pid;
+  int status;
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
 
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
 
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
+  // loop through all the results and bind to the first we can
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                         p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
 
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                   sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
 
-		break;
-	}
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
 
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		return 2;
-	}
+    break;
+  }
 
-	freeaddrinfo(servinfo); // all done with this structure
+  if (p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    return 2;
+  }
 
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
+  freeaddrinfo(servinfo); // all done with this structure
 
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
 
-	printf("server: waiting for connections...\n");
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
 
-	while(1) {  // main accept() loop
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
+  printf("server: waiting for connections...\n");
 
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("server: got connection from %s\n", s);
-	int guard = 1;	        
-		while(guard){
-		char buf[500];
-  		int numbytes;      	
-		do{
-                if ((numbytes = recv(new_fd, buf, 500-1, 0)) == -1) {
-                        perror("recv");
-                        exit(1);
-                	}
-		}
-		while(buf[0] != 'c');
-        	buf[numbytes] = 0;
-		printf(buf);
-		switch(buf[1]){
-		case 'l':
-		if (!fork()) { // this is the child process
-			close(sockfd); // child doesn't need the listener
-			dup2(new_fd, 1);
-			close(new_fd);
-			execl("/usr/bin/ls", "ls", (char *) NULL);
-			//close(new_fd);
-			exit(0);
-		}
-		break;
-		case 'q':
-		default:
-		guard = 0;
-		close(new_fd);
-		break;
-		
-		}
-		write(new_fd, 0 , 1);
+  while (1) { // main accept() loop
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
 
-}
-}
-	return 0;
+    inet_ntop(their_addr.ss_family,
+              get_in_addr((struct sockaddr *)&their_addr),
+              s, sizeof s);
+    printf("server: got connection from %s\n", s);
+
+    if (!fork()) {
+      close(sockfd);
+      if ((numbytes = recv(new_fd, rec_cmd, 100 - 1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+      }
+
+      rec_cmd[numbytes] = '\0';
+
+      if (!strcmp(rec_cmd, "list")) /// list contents and send back////
+      {
+        printf("hi from ls cmd\n");
+
+
+        //ls command through child
+        if ((pid = fork()) == 0) {
+          dup2(new_fd, 1);
+          execl("/usr/bin/ls", "ls", (char *)NULL);
+          error("could not exec 'ls'");
+        }
+
+        wait(&status);
+        exit(0);
+      }
+      printf("Hi bill: '%s'\n", rec_cmd);
+
+      if (!strcmp(rec_cmd, "disp")) /// display file and send back ////
+      {
+
+        if ((numbytes = recv(new_fd, rec_cmd, 100 - 1, 0)) == -1)
+        {
+          perror("recvdisplay"); exit;
+        }
+        rec_cmd[numbytes] = '\0'; printf("got filename\n");
+
+        dup2(new_fd, 1);
+        //execl("/user/bin/less", rec_cmd,(char *)NULL);
+        //error("not displaying 'less'");
+
+        FILE *fp;
+        int fsize;
+        char dis[100];
+
+        if ((numbytes = recv(new_fd, rec_cmd, 100 - 1, 0)) == -1)
+        {
+          perror("recvdisplay"); exit;
+        }
+        rec_cmd[numbytes] = '\0'; printf("got filename\n");
+        //dis = rec_cmd;
+        printf("%s\n", rec_cmd);
+        if (!fork())
+        {
+          //dup2(new_fd,1);
+          //fp = fopen(rec_cmd,"r");
+          while (1)
+          {
+            dup2(new_fd, 1);
+            fsize = fgetc(fp);
+            if (feof(fp))break;
+            printf("%c", fsize);
+          }
+          printf("from display\n");
+
+          fclose(fp);
+          printf("\n");
+        }
+
+
+      }
+
+      //Check command
+      if (!strcmp(rec_cmd, "chek"))
+      {
+        FILE *fp;
+        char filename[1000];
+        int numb;
+
+        if ((numb = recv(new_fd, filename, 100 - 1, 0)) == -1) {
+          perror("recv");
+          exit(1);
+        }
+        filename[numb] = '\0';
+
+        printf("Looking for %s\n", filename);
+
+        fp = fopen(filename, "r");
+        if (fp == NULL)
+          send(new_fd, "no", 2, 0);
+        else
+          send(new_fd, "yes", 3, 0);
+
+      }
+
+      close(new_fd);
+      exit(0);
+    }//end fork
+    close(new_fd);
+
+
+
+  }
+
+  return 0;
 }
 
